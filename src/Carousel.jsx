1c8de91f8d2981dcb3371dac20b426d663eef238
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-25 14:08:12 +0200

    feat(handle-events-class-components):  "this" used to access method, no need for arrow function, event asumed from declaration, eslint disable

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,20 +9,29 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (event) => {
+    // using "handle" in naming is good practice, visually cue that this function is an event handler
+    this.setState({
+      active: +event.target.dataset.index, // "+" unary plus, converts string to number, javascript thing...
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
-
-    console.log({ images });
 
     return (
       <div className="carousel">
         <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, idx) => (
+            //elements that are clickable should be wrapped in button or <a> tag or are otherwise inaccesible
+            // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
               key={photo}
               src={photo}
+              data-index={idx}
               className={idx === active ? "active" : ""}
               alt="animal thumbnail"
             />
