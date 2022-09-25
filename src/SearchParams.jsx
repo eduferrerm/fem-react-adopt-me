7e78019ff4b9476a1068a8e90dc0e6e4e95c7495
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-25 19:27:11 +0200

    feat(context-state): created dropdown with values that when selected change value of theme color being passed through context

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -10,7 +10,7 @@ export const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [breeds] = useBreedList(animal);
-  const [theme] = useContext(ThemeContext);
+  const [theme, setTheme] = useContext(ThemeContext);
   // setTheme is availble to mutate theme value, like useState, [theme, setTheme], we just aren't using it, so no need to declare / destructure it
 
   const [pets, setPets] = useState([]);
@@ -87,6 +87,20 @@ export const SearchParams = () => {
                 {breed}
               </option>
             ))}
+          </select>
+        </label>
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}
+          >
+            <option value=""></option>
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
           </select>
         </label>
         <button style={{ backgroundColor: theme }}>Submit</button>
